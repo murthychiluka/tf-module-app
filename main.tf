64186f18fefb5ea6afc2b048b53ca89bba7637a5
Murthy <murthychiluka@gmail.com>
2023-03-24 17:48:45 +0530

    d

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_launch_template" "main" {
-     name = "${var.component}-${var.env}"
+  name = "${var.component}-${var.env}"
 
 
-#   iam_instance_profile {
-#     name = "test"
-#   }
+  #   iam_instance_profile {
+  #     name = "test"
+  #   }
 
   image_id = "data.aws_ami.ami.id"
 
@@ -14,18 +14,27 @@ resource "aws_launch_template" "main" {
 
   instance_type = var.instance_type
 
-  }
+}
 
-  tag_specifications {
-    resource_type = "instance"
+tag_specifications {
+  resource_type = "instance"
 
-    tags = merge(
-      var.tags,
-      { Name = "${var.component}-${var.env}" }
-    )
-  }
+  tags = merge(
+    var.tags,
+    { Name = "${var.component}-${var.env}" }
+  )
+}
 
 #   user_data = filebase64("${path.module}/example.sh")
 
+resource "aws_autoscaling_group" "main" {
+    desired_capacity   = var.desired.capacity
+    max_size           = var.max_size
+    min_size           = var.min_size
+    vpc_zone_identifier = var.subnets
 
-
+ launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  } 
+}
