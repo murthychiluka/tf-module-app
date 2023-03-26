998a6a5dd96fbf7ee61c9483f9399debf5fa443a
Murthy <murthychiluka@gmail.com>
2023-03-26 15:34:13 +0530

    d

--- userdata.sh
+++ userdata.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-pull -i localhost, -U https://github.com/raghudevopsb71/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/murthychiluka/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
