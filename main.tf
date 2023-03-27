e182bcc3b9a2a7c6d089f23837cc3f1ecb32cf3d
Murthy <murthychiluka@gmail.com>
2023-03-27 14:45:12 +0530

    d

--- main.tf
+++ main.tf
@@ -106,13 +106,13 @@ resource "aws_lb_target_group" "main" {
     { Name = "${var.component}-${var.env}" }
   )
 }
-# resource "aws_route53_record" "main" {
-#   zone_id = data.aws_route53_zone.domain.zone_id
-#   name    = local.dns_name
-#   type    = "CNAME"
-#   ttl     = 30
-#   records = [var.alb_dns_name]
-# }
+resource "aws_route53_record" "main" {
+  zone_id = data.aws_route53_zone.domain.zone_id
+  name    = local.dns_name
+  type    = "CNAME"
+  ttl     = 30
+  records = [var.alb_dns_name]
+}
 
 
 
