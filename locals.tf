bd105ff1a72defceecdb237184ac661fbb059fff
Murthy <murthychiluka@gmail.com>
2023-03-27 19:54:18 +0530

    d

--- locals.tf
+++
@@ -1,9 +0,0 @@
-#dns_name = var.component == "frontend" ?" ? "${var.env}.${var.dns_domain}" : "${var.component}-${var.env}.${var.dns_domain}"
-
-
-locals {
-  dns_word = var.env == "prod" ? "www" : var.env
-  dns_name = var.component == "frontend" ? "${local.dns_word}.${var.dns_domain}" : "${var.component}-${var.env}.${var.dns_domain}"
-}
-
-#
