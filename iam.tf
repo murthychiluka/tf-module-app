aceb7a57cab90eb077a40708072867a98c6661bd
Murthy <murthychiluka@gmail.com>
2023-03-29 20:57:06 +0530

    d

--- iam.tf
+++ iam.tf
@@ -15,13 +15,14 @@ resource "aws_iam_policy" "main" {
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource" : [
-          "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${var.component}*",
+        # "Resource" : [
+        #   "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${var.component}*",
 
-          "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.docdb.*",
+        #   "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.docdb.*",
 
-          "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.elasticache.*"
-        ]
+        #   "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.elasticache.*"
+        # ]
+        Resource : [for k in local.parameters : "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${k}.*"]
       },
       {
         "Sid" : "VisualEditor1",
@@ -32,6 +33,7 @@ resource "aws_iam_policy" "main" {
     ]
   })
 }
+
 
 resource "aws_iam_role" "main" {
   name = "${var.component}-${var.env}"
