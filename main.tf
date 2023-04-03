d9e1c99c7367dbaf81607d3507f2d5f59ba3cd5a
Murthy <murthychiluka@gmail.com>
2023-04-03 14:49:42 +0530

    d

--- main.tf
+++ main.tf
@@ -46,6 +46,19 @@ resource "aws_autoscaling_group" "main" {
 
 
   }
+
+  #   resource "aws_autoscaling_policy" "asg-cpu-rule" {
+  #   name                   = "CPULoadDetect"
+  #   autoscaling_group_name = aws_autoscaling_group.main.name
+  #   policy_type            = "TargetTrackingScaling"
+  #   target_tracking_configuration {
+  #     predefined_metric_specification {
+  #       predefined_metric_type = "ASGAverageCPUUtilization"
+  #     }
+  #     target_value = 40.0
+  #   }
+  # }
+
   #   tags = merge(
   #     var.tags,
   #     { Name = "${var.component}-${var.env}" }
